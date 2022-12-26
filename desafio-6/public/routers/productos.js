b97bf741df1fd4c321fc5b9ffa9ca9a62eee994a
Andres Coria <andrescoria1984@gmail.com>
2022-12-26 19:25:29 -0300

    desafio 6

--- desafio-6/public/routers/productos.js
+++ desafio-6/public/routers/productos.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const router = Router();
-const Contenedor = require("../controllers/productsController.js");
+const Contenedor = require("../../controllers/productsController.js");
 const productos = new Contenedor("./controllers/productos.json");
 const notFound = { error: "Producto no encontrado" };
 
@@ -15,10 +15,10 @@ const notFound = { error: "Producto no encontrado" };
 router.get("/", async (req, res) => {
     const arrayProductos = await productos.getAll();
     console.log(arrayProductos);
-    res.render("lista", {
+    res.render("formulario", {
         productos: arrayProductos,
-        style: "lista.css",
-        title: "Lista de productos Handlebars",
+        style: "formulario.css",
+        title: "Productos con Handlebars",
     });
 });
 
